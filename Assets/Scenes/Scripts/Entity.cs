a26b8ee186f5cfe60230b4eae655b69079f8b4e2
Daderyn <kittyboobooloves@gmail.com>
2024-11-04 18:53:40 -0800

    enemyplayerwkshp

--- Assets/Scenes/Scripts/Entity.cs
+++ Assets/Scenes/Scripts/Entity.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.Events;
+using System.Runtime.CompilerServices;
 
 public class Entity : MonoBehaviour
 {
     [Serializable]
     public struct EntityStats
-    {
-        public float MovementSpeed;
-    }
+        {
+         public float MaxHealth;
+            public float MovementSpeed;
+        }
+
+    [NonSerialized]
+    public float CurrentHealth;
+    bool dead = false;
 
     public EntityStats stats;
+
+    private void Start()
+         {
+              CurrentHealth = stats.MaxHealth;
+         }
+
+    public void TakeDamage(float damage)
+         {
+            CurrentHealth -= Mathf.Ceil(damage);
+
+            if(CurrentHealth <= 0)
+            {
+                Die();
+            }
+        }
+    
+    private void Die()
+        {
+            dead = true;
+            Destroy(gameObject);
+        }
+
 }
